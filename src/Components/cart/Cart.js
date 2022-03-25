9651a1d2e979580a121a5d7e1df08b0f4bc119fb
Arick Hasan <hasanarick000@gmail.com>
2022-03-25 18:18:21 +0600

    cart price update

--- src/Components/cart/Cart.js
+++ src/Components/cart/Cart.js
@@ -2,11 +2,22 @@ import React from "react";
 import "./Cart.css";
 
 const Cart = ({ cart }) => {
+  let price = 0;
+  let shipping = 0;
+  for (const product of cart) {
+    price = price + product.price;
+    shipping = shipping + product.shipping;
+  }
+  const tax = (price * 0.1).toFixed(2);
   console.log(cart);
   return (
     <div className="order">
       <h3>Order Summary</h3>
-      <p>selected item : {cart.length}</p>
+      <p>Selected item : {cart.length}</p>
+      <p>Total Price : ${price}</p>
+      <p>Total Shipping : ${shipping}</p>
+      <p>Tax : ${tax}</p>
+      <h4>Grand Total : </h4>
     </div>
   );
 };
